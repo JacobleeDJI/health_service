44a197e749924db2caad00f867bbaaa420cb641d
Jacoblee <jacobleeloveru@gmail.com>
2017-06-28 10:39:31 +0800

    新增验证码功能，正在开发中

---
+++ src/main/java/com/model/Sys.java
@@ -0,0 +1,34 @@
+package com.model;
+
+/**
+ * Created by jacob on 2017/6/28.
+ */
+public class Sys {
+    private int id;
+    private String name;
+    private String value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
